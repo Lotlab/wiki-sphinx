d25dec3a40c1b0a2af111b1411a935d4d1265bca
Jiangming1399 <jiangming1399@163.com>
2020-03-28 19:09:18 +0800

    似乎是只能手动构建了

---
+++ docs/_static/custom.js
@@ -0,0 +1,50 @@
+
+let tables = document.getElementsByTagName("table");
+for(let i=0;i<tables.length;i++) {
+    tables[i].classList.add("mui-table","mui-table--bordered");
+}
+
+var sidedrawer = document.getElementById("sidedrawer");
+
+document.getElementsByClassName("js-show-sidedrawer")[0].onclick = showSideDrawer;
+document.getElementsByClassName("js-hide-sidedrawer")[0].onclick = hideSideDrawer;
+
+document.getElementById("toc-hide").onclick = function(){
+    document.getElementById("toc-content").classList.toggle("mui--hide");
+    return false;
+}
+
+let foldEls = document.getElementsByClassName("toctree-l1 current");
+for (let i = 0; i < foldEls.length; i++) {
+    let sb = foldEls[i].firstChild.nextElementSibling;
+    if (sb != undefined) {
+        sb.classList.add("element-hidden");
+        foldEls[i].firstChild.onclick = foldElementHandler;
+    }
+}
+
+function showSideDrawer() {
+    var options = {
+        onclose: function() {
+            sidedrawer.classList.remove("active");
+            document.body.append(sidedrawer);
+            $sidedrawerEl
+                .removeClass('active')
+                .appendTo(document.body);
+        }
+    };
+    let overlayEl = mui.overlay('on', options);
+    overlayEl.append(sidedrawer);
+    setTimeout(function() {
+        sidedrawer.classList.add("active");
+    }, 20);
+}
+
+function hideSideDrawer() {
+    document.body.classList.toggle("hide-sidedrawer");
+}
+
+function foldElementHandler(ev) {
+    ev.target.nextElementSibling.classList.toggle("element-hidden");
+    return false;
+}
